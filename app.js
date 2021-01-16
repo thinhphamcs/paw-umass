cfeb568e2c4f4ad08fd319ab267336e11986fe9e
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-15 21:50:54 -0500

    Added .env file for a better way to connect to database

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 // Import require
 const express = require("express"); // Start server from nodejs
-const mysql = require("mysql");
+const mysql = require("mysql"); // For MySQL database
+const dotenv = require("dotenv"); // For more secure way to connect to database
+
+/**
+ * Tell dotenv where is the variables I want
+ * ./ means the same directory
+ * The file .env can be name anything such as password.env or stuff.env
+ */
+dotenv.config({
+    path: './.env',
+});
 
 // Start server with this "app"
 const app = express();
@@ -12,19 +22,19 @@ const app = express();
  * MAMPP/WAMPP by default using 'root' as 'user' and 'root' as 'password'
  */
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodejs-login'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
 });
 
 // Connect to database
 db.connect((err) => {
     if (err) {
-        console.log(err);
+        console.log(err); // Should print the error message
     }
     else {
-        console.log("MySQL Connected...");
+        console.log("MySQL Connected..."); // Should print this message in console to prove it is connected
     }
 });
 
