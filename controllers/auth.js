c4e947c7d051ad82b70b07c79e6028d08b1d52ad
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-17 21:48:40 -0500

    Better way to write same variable names with the same req.body.name

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,17 @@
 exports.register = (req, res) => {
     console.log(req.body);
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const email = req.body.email;
-    const password = req.body.password;
-    const passwordConfirm = req.body.passwordConfirm;
-    const phone = req.body.phone;
+
+    /**
+     * Better way to write this 
+     * const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const email = req.body.email;
+        const password = req.body.password;
+        const passwordConfirm = req.body.passwordConfirm;
+        const phone = req.body.phone;
+     */
+    const { firstName, lastName, email, password, passwordConfirm, phone } = req.body;
+
+
     res.send("Form Submitted");
 }
