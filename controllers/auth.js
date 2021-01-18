7f7b41003db5863c78bc2be005484ba3cae34c7a
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-18 12:30:56 -0500

    Added cookie functionality to auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -79,6 +79,15 @@ exports.login = async (req, res) => {
                     expiresIn: process.env.JWT_EXPIRES_IN
                 });
                 console.log("The token is: " + token);
+
+                const cookieOptions = {
+                    expires: new Date(
+                        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+                    ),
+                    httpOnly: true,
+                }
+                res.cookie('jwt', token, cookieOptions);
+                res.status(200).redirect("/");
             }
         });
     } catch (err) {
