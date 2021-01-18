bc90dd613b873fd22e328123bac412a30e021cac
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-17 21:43:17 -0500

    Added variable names to store the data from form

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,10 @@
 exports.register = (req, res) => {
     console.log(req.body);
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const password = req.body.password;
+    const passwordConfirm = req.body.passwordConfirm;
+    const phone = req.body.phone;
     res.send("Form Submitted");
 }
