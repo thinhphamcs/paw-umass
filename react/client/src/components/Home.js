16f3ff8b1a42d5563aebb7dc7bb45266d4e150a7
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-18 22:44:27 -0500

    Added template code for home.js

--- react/client/src/components/Home.js
+++ react/client/src/components/Home.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-
+        <div className="container">
+            <h1 className="title">All users</h1>
+            <ul>
+                <li><b>Name:</b> Thinh / <b>Email:</b> thinh@gmail.com</li>
+                <li><b>Name:</b> Thinh / <b>Email:</b> thinh@gmail.com</li>
+            </ul>
         </div>
     )
 }
