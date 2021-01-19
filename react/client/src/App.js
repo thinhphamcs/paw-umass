48c85396a156f67393bcab8b43b36aed6f5b18c0
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-18 22:24:31 -0500

    Added Nav, Switch, and Route component

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    /**
+     * BrowserRouter will allow to navigate through different pages with react 
+     * Switch will make sure we only render one component per url
+     */
+    // 
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Form} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
