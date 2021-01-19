88c4a9590dba5f15588af74d50d46baaa740aab0
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-18 22:31:09 -0500

    Finished basic nav component

--- react/client/src/components/Nav.js
+++ react/client/src/components/Nav.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Nav = () => {
     return (
-        <div>
-
-        </div>
+        <nav>
+            <h2>MERN</h2>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
