430626f7c5ee27b5306f5c32d0efc603d39a3e40
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-18 23:31:16 -0500

    Added basic template code for register

--- react/client/src/components/Register.js
+++ react/client/src/components/Register.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-
+        <div className="container">
+            <h1 className="title">Register User</h1>
+            <form>
+                <label>Name: </label>
+                <input required type="text" id="userName" name="userName"></input>
+                <label>Email: </label>
+                <input required type="email" id="userEmail" name="userEmail"></input>
+                <button type="submit">Register</button>
+            </form>
         </div>
     )
 }
