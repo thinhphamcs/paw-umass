22d3fb70561b40cb42c26fb36e4da799911e3c8d
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-20 15:14:49 -0500

    Added path link for the burger menu

---
+++ react/client/src/components/SideBarData.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SideBarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        className: 'nav-text'
+    },
+    {
+        title: 'Profile',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        className: 'nav-text'
+    },
+    {
+        title: 'Donate',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        className: 'nav-text'
+    },
+    {
+        title: 'Logout',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        className: 'nav-text'
+    },
+];
