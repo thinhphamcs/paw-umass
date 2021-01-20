7b91ae6e5e8cbc35e857bb2dde2d460bfee41366
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-19 20:16:04 -0500

    Added backend functionality for register.js

--- react/client/src/components/Register.js
+++ react/client/src/components/Register.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css';
 const Register = () => {
+    const [userDetails, setUserDetails] = useState({
+        userName: '',
+        userEmail: '',
+        message: ''
+    });
+
+    const formValues = (event) => {
+        setUserDetails({
+            ...userDetails,
+            [event.target.name]: event.target.value
+        });
+    }
     return (
         <div className="container">
             <h1 className="title">Register User</h1>
             <form>
                 <label>Name: </label>
-                <input required type="text" id="userName" name="userName"></input>
+                <input required type="text" id="userName" name="userName" onChange={formValues}></input>
                 <br />
                 <label>Email: </label>
-                <input required type="email" id="userEmail" name="userEmail"></input>
+                <input required type="email" id="userEmail" name="userEmail" onChange={formValues}></input>
                 <br />
                 <button type="submit">Register</button>
             </form>
