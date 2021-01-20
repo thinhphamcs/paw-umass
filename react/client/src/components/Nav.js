d27e5a05d6756a0fd4acde0c644a82dda92b19f6
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-20 15:09:28 -0500

    Added burger icon and functionality to navbar

--- react/client/src/components/Nav.js
+++ react/client/src/components/Nav.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import logo from '../logo.png';
 
 const Nav = () => {
+    const { sidebar, setSideBar } = useState(false);
+
+    const showSideBar = () => setSideBar(!sidebar);
     return (
         <nav>
-            <h2>LOGO HERE</h2>
-
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
-            </ul>
+            <div className="logoImage">
+                <img src={logo} alt="Logo" />
+            </div>
+            <div className='navbar'>
+                <Link to='#' className='menu-bars'>
+                    <FaIcons.FaBars onClick={showSideBar} />
+                </Link>
+            </div>
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items'>
+                    <li className="navbar-toggle">
+                        <Link to="#" className='menu-bars'>
+                            <AiIcons.AiOutlineClose />
+                        </Link>
+                    </li>
+                    {/* <li>
+                        <Link to="/register">Register</Link>
+                    </li> */}
+                </ul>
+            </nav>
         </nav>
     )
 }
