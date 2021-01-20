05cd515d3f292abacfe25d6a1586f72e02358756
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-19 20:34:29 -0500

    Fixing backend functionality for register.js and added setupProxy

--- react/client/src/components/Register.js
+++ react/client/src/components/Register.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import './Register.css';
+import axios from 'axios';
 const Register = () => {
     const [userDetails, setUserDetails] = useState({
-        userName: '',
-        userEmail: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        phone: '',
         message: ''
     });
 
@@ -13,15 +17,42 @@ const Register = () => {
             [event.target.name]: event.target.value
         });
     }
+    // This is what will be sending to backend
+    const register = async (event) => {
+        event.preventDefault(); // when you submit a form by default you are reloading the page or go somewhere so this will prevent it
+        const body = JSON.stringify({
+            firstName: userDetails.firstName,
+            lastName: userDetails.lastName,
+            email: userDetails.email,
+            password: userDetails.password,
+            phone: userDetails.phone
+        });
+        // Using post method to send to database
+        const response = await axios.post("/auth/register", body, {
+            header: {
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log(response);
+    }
     return (
         <div className="container">
             <h1 className="title">Register User</h1>
-            <form>
-                <label>Name: </label>
-                <input required type="text" id="userName" name="userName" onChange={formValues}></input>
+            <form onSubmit={register}>
+                <label>First: </label>
+                <input required type="text" id="firstName" name="firstName" onChange={formValues}></input>
+                <br />
+                <label>Last: </label>
+                <input required type="text" id="lastName" name="lastName" onChange={formValues}></input>
                 <br />
                 <label>Email: </label>
-                <input required type="email" id="userEmail" name="userEmail" onChange={formValues}></input>
+                <input required type="email" id="email" name="email" onChange={formValues}></input>
+                <br />
+                <label>Password: </label>
+                <input required type="text" id="password" name="password" onChange={formValues}></input>
+                <br />
+                <label>Phone: </label>
+                <input required type="text" id="phone" name="phone" onChange={formValues}></input>
                 <br />
                 <button type="submit">Register</button>
             </form>
