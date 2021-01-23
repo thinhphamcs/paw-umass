d3c9da68e589877b57824b041704034847330a33
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-22 23:46:50 -0500

    Added logos and prevent phone to rotate

--- react/client/src/components/Choose.js
+++ react/client/src/components/Choose.js
@@ -1,7 +1,9 @@
 // Import
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import './Choose.css';
+import Logo from '../logo3.png';
+
 
 // Function
 const Choose = () => {
@@ -19,13 +21,25 @@ const Choose = () => {
     return (
         // Determine which page to redirect
         <div className="container">
-            <button onClick={() => setPage("Login")}>
-                Login
-            </button>
-            <br />
-            <button onClick={() => setPage("Register")}>
-                Register
-            </button>
+            <div className="container-header">
+                <img src={Logo} alt="Logo" />
+            </div>
+            <div className="container-body">
+                <h2>See what's going on</h2>
+                <br />
+                <h4>Join PawUMass today</h4>
+                <br />
+                <button className="login-button" onClick={() => setPage("Login")}>
+                    Login
+                </button>
+                <button className="register-button" onClick={() => setPage("Register")}>
+                    Register
+                </button>
+            </div>
+            <div className="container-footer">
+
+            </div>
+
         </div>
     )
 }
