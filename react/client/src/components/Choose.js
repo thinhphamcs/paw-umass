2314f324a743d524bac9a5b9d5011f6227afa810
Ethan Pham <phamtuongthinh@gmail.com>
2021-01-25 20:22:44 -0500

    Finished CSS and styling for choose page

--- react/client/src/components/Choose.js
+++ react/client/src/components/Choose.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { Redirect, Link } from 'react-router-dom';
 import './Choose.css';
 import Logo from '../logo3.png';
-
+import * as FaIcons from 'react-icons/fa';
 
 // Function
 const Choose = () => {
@@ -20,28 +20,53 @@ const Choose = () => {
     }
     return (
         // Determine which page to redirect
-        <div className="container">
+        <>
             <div className="container-header">
                 <img src={Logo} alt="Logo" />
             </div>
             <div className="container-body">
-                <h2>See what's going on</h2>
-                <br />
-                <h4>Join PawUMass today</h4>
-                <br />
-                <button className="login-button" onClick={() => setPage("Login")}>
-                    Login
-                </button>
-                <button className="register-button" onClick={() => setPage("Register")}>
-                    Register
-                </button>
+                <div className="intro">
+                    <h2>See what's going on</h2>
+                    <br />
+                    <h4>Join PawUMass today</h4>
+                    <br />
+                </div>
+                <div className="buttons">
+                    <button className="login-button" onClick={() => setPage("Login")}>
+                        Login
+                    </button>
+                    <button className="register-button" onClick={() => setPage("Register")}>
+                        Register
+                    </button>
+                </div>
             </div>
             <div className="container-footer">
-
+                <Link to="#" className='list-items'>
+                    
+                        About
+                    
+                </Link>
+                <Link to="#" className='list-items'>
+                    
+                        Terms of Service
+                    
+                </Link>
+                <Link to="#" className='list-items'>
+                    
+                        Privacy Policy
+                    
+                </Link>
+                <Link to="#" className='list-items'>
+                    
+                        Cookie Policy
+                    
+                </Link>
+                <p>
+                    <FaIcons.FaRegCopyright /> 2021 PawUMass
+                </p>
             </div>
-
-        </div>
-    )
+        </>
+    );
 }
 
 export default Choose;
