7d4bddca33ed08c517254d2c34b378f869244ae0
Ethan Pham <phamtuongthinh@gmail.com>
2021-02-23 20:51:00 -0500

    Added CSS

--- react/client/src/layout/Register/Register.js
+++ react/client/src/layout/Register/Register.js
@@ -1,34 +1,47 @@
 // Import
 import React from 'react';
+import Logo from '../../assets/images/logo3.png';
+import { Link } from 'react-router-dom';
 import './Register.css';
-import { Button, Form } from "react-bootstrap";
 
 // This will be the font end with props I can use to display data
 function RegisterUI({ form: { onChange, form, registerFormValid, onSubmit, loading, error } }) {
 
     return (
-        <form>
-            {error ? <div>{error.message}</div> : null}
-            <Form.Label>First: </Form.Label>
-            <Form.Control required type="text" id="firstName" name="firstName" value={form.firstName} onChange={onChange}></Form.Control>
-            <br />
-            <Form.Label>Last: </Form.Label>
-            <Form.Control required type="text" id="lastName" name="lastName" value={form.lastName} onChange={onChange}></Form.Control>
-            <br />
-            <Form.Label>Email: </Form.Label>
-            <Form.Control required type="email" id="email" name="email" value={form.email} onChange={onChange}></Form.Control>
-            <br />
-            <Form.Label>Password: </Form.Label>
-            <Form.Control required type="password" id="password" name="password" value={form.password} onChange={onChange}></Form.Control>
-            <br />
-            <Form.Label>Confirm Password: </Form.Label>
-            <Form.Control required type="password" id="passwordConfirm" name="passwordConfirm" value={form.passwordConfirm} onChange={onChange}></Form.Control>
-            <br />
-            <Form.Label>Phone: </Form.Label>
-            <Form.Control required type="text" id="phone" name="phone" value={form.phone} onChange={onChange}></Form.Control>
-            <br />
-            <Button onClick={onSubmit} disabled={registerFormValid || loading} loading={loading} type="submit">Register</Button>
-        </form >
+        <div className="register-container">
+            <div className="register-container-header">
+                <img src={Logo} alt="Logo" />
+            </div>
+            <h1 className="register-form-title">Create your account </h1>
+            <form className="register-form-container">
+                {error ? <div>{error.message}</div> : null}
+                <div className="register-form-group">
+                    <input required className="register-input" type="text" id="firstName" name="firstName" placeholder="First Name" value={form.firstName} onChange={onChange}></input>
+                </div>
+                <div className="register-form-group">
+                    <input required className="register-input" type="text" id="lastName" name="lastName" placeholder="Last Name" value={form.lastName} onChange={onChange}></input>
+                </div>
+                <div className="register-form-group">
+                    <input required className="register-input" type="email" id="email" name="email" placeholder="Email" value={form.email} onChange={onChange}></input>
+                </div>
+                <div className="register-form-group">
+                    <input required className="register-input" type="password" id="password" name="password" placeholder="Password" value={form.password} onChange={onChange}></input>
+                </div>
+                <div className="register-form-group">
+                    <input required className="register-input" type="password" id="passwordConfirm" name="passwordConfirm" placeholder="Password Confirm" value={form.passwordConfirm} onChange={onChange}></input>
+                </div>
+                <div className="register-form-group">
+                    <input required className="register-input" type="text" id="phone" name="phone" placeholder="Phone" value={form.phone} onChange={onChange}></input>
+                </div>
+                <button className="register-form-button" onClick={onSubmit} disabled={registerFormValid || loading} loading={loading} type="submit">Register</button>
+            </form>
+            <div className="register-container-footer">
+                Already have an account?
+                <Link to="/login" className='register-list-items' >
+                    Sign in
+                </Link>
+            </div>
+        </div>
     )
 }
 
