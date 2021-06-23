d3477efca43b65d7236705d177d7ffc3cff32d06
Ethan Pham <phamtuongthinh@gmail.com>
2021-06-22 21:22:47 -0400

    Ready for deployment

---
+++ config/config.js
@@ -0,0 +1,11 @@
+// Import dotenv here so Sequelize CLI to access this
+require('dotenv').config('../.env');
+// Exports
+module.exports = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT
+}
