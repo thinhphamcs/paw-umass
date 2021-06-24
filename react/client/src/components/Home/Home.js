a4cf847818474acf6a5d1172649276541888fc8b
Ethan Pham <phamtuongthinh@gmail.com>
2021-06-24 16:37:07 -0400

    Update nodejs from 12 to 14

--- react/client/src/components/Home/Home.js
+++ react/client/src/components/Home/Home.js
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import HomeUI from '../../layout/Home/Home';
-// import { HomeForm } from '../Home/HomeForm';
+import { HomeForm } from '../Home/HomeForm';
 // Export it as a form so we can use it as props
 const Home = () => {
     const history = useHistory();
@@ -10,8 +10,7 @@ const Home = () => {
         document.body.style.backgroundColor = "white";
     }, [history]);
     return (
-        // form={HomeForm()}
-        <HomeUI />
+        <HomeUI form={HomeForm()} />
     );
 }
 export default Home;
